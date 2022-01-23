23d2584a78bb0b50aedd8b876574cb761d8321ac
cbuiles <cgomezb@unal.edu.co>
2022-01-23 16:50:53 -0500

    Clase 08. Crear controller Producto y la vista Productos

---
+++ src/main/java/com/curso/ecommerce/service/ProductoService.java
@@ -0,0 +1,18 @@
+package com.curso.ecommerce.service;
+
+import com.curso.ecommerce.model.Producto;
+
+import java.util.Optional;
+
+public interface ProductoService {
+
+    public Producto save(Producto producto);
+
+//    Retorna objeto Optional pq nos da la opcion de corroborar si nos devuelve el objeto
+    public Optional<Producto> get(Integer id);
+
+    public void update(Producto producto);
+
+    public void delete(Integer id);
+
+}
