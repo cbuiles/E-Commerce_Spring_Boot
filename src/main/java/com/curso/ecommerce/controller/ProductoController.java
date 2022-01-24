4a2da32bfe1be34854ac068ed6ee16908da0a0a9
cbuiles <cgomezb@unal.edu.co>
2022-01-23 20:31:57 -0500

    Clase 12. Implementar la funcioanilidad mostrar productos administrador

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,9 +1,13 @@
 package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.model.Producto;
+import com.curso.ecommerce.model.Usuario;
+import com.curso.ecommerce.service.ProductoService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +19,14 @@ public class ProductoController {
 //    Para obtener los Logs de nuestro controlador
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
-    @GetMapping("")
-    public String show(){
+    @Autowired
+    private ProductoService productoService;
 
+    @GetMapping("")
+//    El objeto Model envia la informacion del Obj del Backend a la vista
+    public String show(Model model){
+
+        model.addAttribute("productos", productoService.findAll());
         return "productos/show";
 
     }
@@ -32,6 +41,12 @@ public class ProductoController {
 
 //        Pruebas para el LOGGER
         LOGGER.info("Este es el objeto producto {}", producto);
+
+        Usuario u = new Usuario(1, "", "", "", "", "", "", "" );
+
+        producto.setUsuario(u);
+
+        productoService.save(producto);
 
         return "redirect:/productos";
 
