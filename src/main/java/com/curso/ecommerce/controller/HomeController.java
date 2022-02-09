3b8696348b2f7bec7b44cae65dae2daafca5d897
cbuiles <cgomezb@unal.edu.co>
2022-02-08 20:37:34 -0500

    Clase 21. Mostrar los datos del producto en otra vista

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.curso.ecommerce.controller;
 
 import com.curso.ecommerce.service.ProductoService;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")//Apunta a la raiz del proyecto
 public class HomeController {
+
+    private final Logger log = LoggerFactory.getLogger(HomeController.class);
+
 
     @Autowired
     private ProductoService productoService;
@@ -20,6 +26,15 @@ public class HomeController {
         modelo.addAttribute("productos", productoService.findAll());
 
         return "usuario/home";
+
+    }
+
+    @GetMapping("productohome/{id}")
+    public String productoHome(@PathVariable("id") Integer id){
+
+        log.info("Id producto enviado como parametro {}", id);
+
+        return "usuario/productohome";
 
     }
 
