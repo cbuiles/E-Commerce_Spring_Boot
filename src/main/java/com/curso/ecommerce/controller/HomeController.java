ed476f6b4f193b0175add0dcb7038a3a822e86fd
cbuiles <cgomezb@unal.edu.co>
2022-02-11 19:29:26 -0500

    Clase 28. Mostrar el carrito desde la opcion del menu

--- src/main/java/com/curso/ecommerce/controller/HomeController.java
+++ src/main/java/com/curso/ecommerce/controller/HomeController.java
@@ -79,7 +79,13 @@ public class HomeController {
         detalleOrden.setTotal(producto.getPrecio() * cantidad);
         detalleOrden.setProducto(producto);
 
-        detalles.add(detalleOrden);
+//        Validar que el producto no se añada 2 veces
+        Integer idProducto = producto.getId();
+        boolean ingresado = detalles.stream().anyMatch(p -> p.getProducto().getId() == idProducto);
+
+        if(!ingresado){
+            detalles.add(detalleOrden);
+        }
 
         sumaTotal= detalles.stream().mapToDouble(dt->dt.getTotal()).sum();
 
@@ -131,6 +137,15 @@ public class HomeController {
 
         return "usuario/carrito";
 
+    }
+
+    @GetMapping("/getCart")
+    public String getCart(Model modelo){
+
+        modelo.addAttribute("cart", detalles);
+        modelo.addAttribute("orden", orden);
+
+        return "usuario/carrito";
     }
 
 }
