5d09f6b222f4a2332147800005cafb18f644f185
cbuiles <cgomezb@unal.edu.co>
2022-02-12 16:40:26 -0500

    Clase 32. Metodo para generar el numero de la orden

--- src/main/java/com/curso/ecommerce/service/IOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/IOrdenService.java
@@ -2,6 +2,9 @@ package com.curso.ecommerce.service;
 
 import com.curso.ecommerce.model.Orden;
 
+import java.util.List;
+
 public interface IOrdenService {
+    List<Orden> findAll();
     Orden save(Orden orden);
 }
