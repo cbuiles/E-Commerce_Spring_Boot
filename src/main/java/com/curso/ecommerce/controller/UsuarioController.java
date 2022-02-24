41bd97b9c94f8c785edddc1751c1f374a515e174
cbuiles <cgomezb@unal.edu.co>
2022-02-23 20:09:35 -0500

    Clase 41. Mostrar la vista compras para el usuario

--- src/main/java/com/curso/ecommerce/controller/UsuarioController.java
+++ src/main/java/com/curso/ecommerce/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -69,6 +70,15 @@ public class UsuarioController {
         }
 
         return "redirect:/";
+
+    }
+
+    @GetMapping("/compras")
+    public String obtenerCompras(Model modelo, HttpSession session){
+
+        modelo.addAttribute("sesion", session.getAttribute("idusuario") );
+
+        return "usuario/compras";
 
     }
 
