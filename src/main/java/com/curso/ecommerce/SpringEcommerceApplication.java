c8ce4316e4aec5c054965ce850903db1c2aab8c1
cbuiles <cgomezb@unal.edu.co>
2022-02-27 20:59:47 -0500

    Clase 48. Seguridades, crear el UserDetailService

--- src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
+++ src/main/java/com/curso/ecommerce/SpringEcommerceApplication.java
@@ -2,6 +2,8 @@ package com.curso.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @SpringBootApplication
@@ -9,6 +11,11 @@ public class SpringEcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringEcommerceApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
